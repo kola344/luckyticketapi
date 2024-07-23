e362db833b5022a84df4d765d2ce4edc632e5e66
kola <koli>
2024-07-23 15:19:47 +0300

    added send feedback request

--- luckyticket_bot/feedback_requests.py
+++ luckyticket_bot/feedback_requests.py
@@ -6,4 +6,3 @@ from aiogram.types import InlineKeyboardButton, KeyboardButton, InlineKeyboardMa
 async def send_feedback_request_to_chat(name, email, phone_number, message):
     text = f'Новая заявка на связь с поддержкой\nИмя: {name}\nЭл. почта, {email}\nТелефон: {phone_number}\n\n{message}'
     await bot.send_message(chat_id=config.feedback_requests_chat, text=text)
-
