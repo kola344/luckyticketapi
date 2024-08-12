84d39335127b7474201b8f533e82464fe13523a5
kola <koli>
2024-08-12 15:56:32 +0300

    edited config url

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 bot_token = '7245054684:AAErp-TrIcbVIxpIG1BCSqEQ_V4FvBti_cY'
-fastapi_url = 'https://luckyticketapi.onrender.com'
+fastapi_url = 'https://api.sv-g.ru'
 webhook_url = f'{fastapi_url}/webhook'
 
 reservations_chat = -4264866215
